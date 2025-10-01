aea264905b6160caeaaf3e509af42c0a62a3da26
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-10-01 19:15:03 -0300

    att

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,30 +1,66 @@
 package com.example.myapp;
 
-import android.app.LocaleManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    EditText nome, peso, altura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        LocaleManager lm;
-        getString(R.string.Hello);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b = findViewById(R.id.btCalculaIMC);
+        nome = findViewById(R.id.Nome);
+        altura = findViewById(R.id.Altura);
+        peso = findViewById(R.id.Peso);
+        b.setOnClickListener(v -> {
+            Intent intent = new Intent(this, IMCResultado.class);
+            //passar os dados para o bundle
+            float peso = Float.parseFloat(edpeso.getText().toString());
+            float altura = Float.parseFloat(edaltura.getText().toString());
+
+            intent.putExtra("altura", altura);
+            intent.putExtra("peso", peso);
+
+            startActivity(intent);
         });
     }
 }
+
+
+/*
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    EditText edpeso, edaltura;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b = findViewById(R.id.btCalculaIMC);
+        edaltura = findViewById(R.id.EdAltura);
+        edpeso = findViewById(R.id.EdPeso);
+        b.setOnClickListener(v -> {
+            Intent intent = new Intent(this, IMCResultado.class);
+            //passar os dados para o bundle
+            float peso = Float.parseFloat(edpeso.getText().toString());
+            float altura = Float.parseFloat(edaltura.getText().toString());
+
+            intent.putExtra("altura", altura);
+            intent.putExtra("peso", peso);
+
+            startActivity(intent);
+        });
+    }
+}
+
+ */
 
 
 
