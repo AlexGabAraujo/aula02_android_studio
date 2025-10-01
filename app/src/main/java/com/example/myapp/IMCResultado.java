4354346a65fd15d8f38db7d4c8464c0e126ec204
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-10-01 19:48:54 -0300

    att

--- app/src/main/java/com/example/myapp/IMCResultado.java
+++ app/src/main/java/com/example/myapp/IMCResultado.java
@@ -1,12 +1,14 @@
 package com.example.myapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IMCResultado extends AppCompatActivity {
+    Button tvButtonInformation, tvButtonHelp;
     TextView tvPeso, tvAltura, tvIMC;
     ImageView tvPerfil;
 
@@ -14,19 +16,27 @@ public class IMCResultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        tvButtonInformation = findViewById(R.id.buttonInformation);
+        tvButtonHelp = findViewById(R.id.buttonHelp);
+
+        /*
         tvPeso = findViewById(R.id.tvPeso);
         tvAltura = findViewById(R.id.tvAltura);
         tvIMC = findViewById(R.id.tvIMC);
 
-        tvPerfil = findViewById(R.id.tvPerfil);
+        tvPerfil = findViewById(R.id.tvPerfil);*/
 
         Bundle b = getIntent().getExtras();
 
         float peso = b.getFloat("peso");
         float altura = b.getFloat("altura");
+        String nome = b.getString("nome");
+        float imc = b.getFloat("imc");
 
-        float imc = peso/(altura*altura);
+        tvButtonInformation
 
+        /*
         tvPeso.setText(Float.toString(peso));
         tvAltura.setText(Float.toString(altura));
         tvIMC.setText(Float.toString(imc));
@@ -48,6 +58,83 @@ public class IMCResultado extends AppCompatActivity {
         }
         else{
             tvPerfil.setImageResource(R.drawable.obesidade3);
-        }
+        }*/
     }
 }
+
+
+/*
+
+private String emailSalvo;
+    private String senhaSalva;
+    private boolean isLoggedIn = false;
+    private Button registerButton, loginButton, menuButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerButton = findViewById(R.id.registerButton);
+        loginButton = findViewById(R.id.loginButton);
+        menuButton = findViewById(R.id.menuButton);
+
+        exibirFragmentoA();
+
+        registerButton.setOnClickListener(v -> exibirFragmentoA());
+        loginButton.setOnClickListener(v -> exibirFragmentoB());
+
+        menuButton.setOnClickListener(v -> {
+            if (isLoggedIn == true) {
+                exibirFragmentoC();
+            } else {
+                Toast.makeText(this, "Faça login para acessar o menu", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void exibirFragmentoA() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.tela, new FragmentoA());
+        ft.commit();
+    }
+
+    private void exibirFragmentoB() {
+        Bundle bundle = new Bundle();
+        bundle.putString("email", emailSalvo);
+        bundle.putString("senha", senhaSalva);
+
+        FragmentoB fragmentoB = new FragmentoB();
+        fragmentoB.setArguments(bundle);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.tela, fragmentoB);
+        ft.commit();
+    }
+
+    private void exibirFragmentoC() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.tela, new FragmentoC());
+        ft.commit();
+    }
+
+    public void salvarDados(String email, String senha) {
+        this.emailSalvo = email;
+        this.senhaSalva = senha;
+
+        Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loginOk() {
+        this.isLoggedIn = true;
+        Toast.makeText(this, "Login realizado com sucesso!", Toast.LENGTH_SHORT).show();
+        exibirFragmentoC();
+    }
+
+    public void loginFalhou() {
+        this.isLoggedIn = false;
+        Toast.makeText(this, "Email ou senha incorretos.", Toast.LENGTH_SHORT).show();
+    }
+}
+
+ */
