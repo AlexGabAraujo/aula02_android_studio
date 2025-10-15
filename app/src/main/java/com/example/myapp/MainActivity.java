b5a2a71e38c664771b895abd487c20d1ad8ff8d7
Alex Araujo <alexgabriel.araujo@hotmail.com>
2025-10-15 20:21:56 -0300

    adicioanndo projeto

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.myapp;
 
+import android.annotation.SuppressLint;
 import android.app.LocaleManager;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,20 +14,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    PackageManager pm;
+    ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        LocaleManager lm;
-        getString(R.string.Hello);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        listView = findViewById(R.id.lista);
+
+        //Recupera gerenciador de pacotes
+        pm=getPackageManager();
+        List<ApplicationInfo> apps = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        AppAdapter adapter = new AppAdapter(this, R.layout.app, apps);
+        listView.setAdapter(adapter);
     }
 }
 
